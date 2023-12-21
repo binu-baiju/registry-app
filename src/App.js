288367f7be20ea196827849b7fce208b1a0460e4
binu-baiju <108217491+binu-baiju@users.noreply.github.com>
2023-12-21 11:08:48 +0530

    add src

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import React from "react"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./routes/Home"
+import Registry from "./routes/Registry"
+import "./App.css";
 
+
+function App() {
+  return (
+    <div className="App">
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} exact/>                 
+          <Route path="/registry" element={<Registry/>} />           
+        </Routes>
+       </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
