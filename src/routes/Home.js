aa2164edc3f1d6f8fe6293fafc2a76e604454403
binu-baiju <108217491+binu-baiju@users.noreply.github.com>
2023-12-21 11:11:56 +0530

    created routes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,14 @@
 
+import React from "react"
+import {Link} from "react-router-dom"
+
+function Home(){
+    return(
+        <div>
+            <h1 style={{color:"white"}}>Home</h1>
+            <Link style={{color:"white"}} to="/registry">Click here to go to registry</Link>
+        </div>
+    )
+}
+
+export default Home
